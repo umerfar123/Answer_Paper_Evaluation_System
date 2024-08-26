c4e0438ce96a73b410c702ffc0379420eb974cf7
Umr_Far <122196783+umerfar123@users.noreply.github.com>
2024-08-26 17:32:07 +0530

    Add files via upload

---
+++ apicall.py
@@ -0,0 +1,11 @@
+
+
+headers = {"Authorization": "Bearer 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"}
+
+API_URL_ST = "https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment-latest"
+
+API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/all-mpnet-base-v2"
+
+API_URL_QS = "https://api-inference.huggingface.co/models/facebook/bart-large-mnli"
+
+API_URL_kw = "https://api-inference.huggingface.co/models/yanekyuk/bert-keyword-extractor"
